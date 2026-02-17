31005087726e504095715a35494b68a05d66a31e
Erlend Vitsø <669804@stud.hvl.no>
2026-02-17 11:24:11 +0100

    modified read functions to divide between raw and preprocessed data

--- src/preprocess/mind_reader.py
+++ src/preprocess/mind_reader.py
@@ -1,20 +1,28 @@
 """Utilities for reading and caching MIND data splits."""
 import pandas as pd
+import os
 
-BASE_PATH = "../../data/"
+BASE_PATH = "../../data"
 
 # Anticipating that the data is stored in "tdt4215-news-recommender/data"
 
-def read_behaviors_train():
-    return pd.read_csv(BASE_PATH + "MINDsmall_train/behaviours.tsv", sep="\t")
+def _read_file(split, filename, raw=False):
+    folder = "raw" if raw else "preprocessed"
+    path = os.path.join(BASE_PATH, folder, split, filename)
 
-def read_behaviors_test():
-    return pd.read_csv(BASE_PATH + "MINDsmall_dev/behaviours.tsv", sep="\t")
+    if not os.path.exists(path):
+        path = os.path.join(BASE_PATH, "raw", split, filename)
 
-def read_news_train():
-    return pd.read_csv(BASE_PATH + "MINDsmall_train/news.tsv", sep="\t")
+    return pd.read_csv(path, sep="\t")
 
-def read_news_test():
-    return pd.read_csv(BASE_PATH + "MINDsmall_test/news.tsv", sep="\t")
+def read_behaviors_train(raw=False):
+    return _read_file("MINDsmall_train", "behaviours.tsv", raw)
 
+def read_behaviors_test(raw=False):
+    return _read_file("MINDsmall_dev", "behaviours.tsv", raw)
 
+def read_news_train(raw=False):
+    return _read_file("MINDsmall_train", "news.tsv", raw)
+
+def read_news_test(raw=False):
+    return _read_file("MINDsmall_test", "news.tsv", raw)
