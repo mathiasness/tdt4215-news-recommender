40baae7191d34d89b51fb00f020649ba7fd173f6
Erlend Vitsø <669804@stud.hvl.no>
2026-02-24 15:08:36 +0100

    ItemkNN CF
    implemented item-based kNN recommender.

---
+++ src/recommenders/__init__.py
@@ -0,0 +1,4 @@
+from src.recommenders.baseline_popular import PopularRecommender
+from src.recommenders.collaborative_filtering import ItemKNNRecommender
+
+__all__ = ["PopularRecommender", "ItemKNNRecommender"]
